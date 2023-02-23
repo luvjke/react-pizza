03350a98fef159ee696b716f2b16b906930d47fc
Nikita <nik_antonov_555@mail.ru>
2023-02-23 16:01:11 +0400

    add Sort

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,25 @@
-function Categories(){
-    return(
-    <div className ="categories">
-    <ul>
-      <li className ="active">Все</li>
-      <li>Мясные</li>
-      <li>Вегетарианская</li>
-      <li>Гриль</li>
-      <li>Острые</li>
-      <li>Закрытые</li>
-    </ul>
-  </div>);
+import React from 'react';
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => (
+          <li
+            key={i}
+            onClick={() => onClickCategory(i)}
+            className={activeIndex === i ? 'active' : ''}>
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 export default Categories;
