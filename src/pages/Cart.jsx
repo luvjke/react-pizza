cf8e83a4069a97f3cad3e9bd7643b476db3d6220
Nikita <nik_antonov_555@mail.ru>
2023-02-26 19:14:28 +0400

    add Pages(Router)

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Cart = () => {
+  return <h1>КОрзина</h1>;
+};
+
+export default Cart;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://63f9e49dbeec322c57e960a3.mockapi.io/items')
+      .then((res) => res.json())
+
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => (
+              <PizzaBlock
+                key={obj.id}
+                title={obj.title}
+                price={obj.price}
+                imageUrl={obj.imageUrl}
+                sizes={obj.sizes}
+                types={obj.types}
+              />
+            ))}
+      </div>
+    </>
+  );
+};
+export default Home;
