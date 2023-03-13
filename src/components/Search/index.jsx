213f89404426b3cb52f81aa1356273ec3eea8eb1
Nikita <nik_antonov_555@mail.ru>
2023-03-13 15:02:13 +0400

    add pagination

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './Search.module.scss';
+
+const Search = ({ searchValue, setSearchValue }) => {
+  return (
+    <div className={styles.root}>
+      <svg
+        className={styles.icon}
+        height="48"
+        viewBox="0 0 48 48"
+        width="48"
+        xmlns="http://www.w3.org/2000/svg">
+        <path d="M31 28h-1.59l-.55-.55C30.82 25.18 32 22.23 32 19c0-7.18-5.82-13-13-13S6 11.82 6 19s5.82 13 13 13c3.23 0 6.18-1.18 8.45-3.13l.55.55V31l10 9.98L40.98 38 31 28zm-12 0c-4.97 0-9-4.03-9-9s4.03-9 9-9 9 4.03 9 9-4.03 9-9 9z" />
+        <path d="M0 0h48v48H0z" fill="none" />
+      </svg>
+      <input
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.target.value)}
+        className={styles.input}
+        placeholder="Поиск пиццы..."
+      />
+      {searchValue && (
+        <svg onClick={() => setSearchValue('')} className={styles.clearIcon}>
+          <g fill="#000000" id="Core" transform="translate(-341.000000, -89.000000)">
+            <g id="close" transform="translate(341.000000, 89.000000)">
+              <path
+                d="M14,1.4 L12.6,0 L7,5.6 L1.4,0 L0,1.4 L5.6,7 L0,12.6 L1.4,14 L7,8.4 L12.6,14 L14,12.6 L8.4,7 L14,1.4 Z"
+                id="Shape"
+              />
+            </g>
+          </g>
+        </svg>
+      )}
+    </div>
+  );
+};
+export default Search;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,32 +4,50 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
 
-const Home = () => {
+const Home = ({ seacrhValue }) => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategotyId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [sortType, setSortType] = React.useState({
     name: 'популярности',
-    sortType: 'rating',
+    sortProperty: 'rating',
   });
 
   React.useEffect(() => {
     setIsLoading(true);
 
-    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
     const sortBy = sortType.sortProperty.replace('-', '');
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
     const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = seacrhValue ? `&search=${seacrhValue}` : '';
+
     fetch(
-      `https://63f9e49dbeec322c57e960a3.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+      `https://63f9e49dbeec322c57e960a3.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
     )
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
-    window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+    //window.scrollTo(0, 0);
+  }, [categoryId, sortType, seacrhValue, currentPage]);
+
+  const pizzas = items.map((obj) => (
+    <PizzaBlock
+      key={obj.id}
+      title={obj.title}
+      price={obj.price}
+      imageUrl={obj.imageUrl}
+      sizes={obj.sizes}
+      types={obj.types}
+    />
+  ));
+
+  const skeleton = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+
   return (
     <div className="container">
       <div className="content__top">
@@ -37,20 +55,8 @@ const Home = () => {
         <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => (
-              <PizzaBlock
-                key={obj.id}
-                title={obj.title}
-                price={obj.price}
-                imageUrl={obj.imageUrl}
-                sizes={obj.sizes}
-                types={obj.types}
-              />
-            ))}
-      </div>
+      <div className="content__items">{isLoading ? skeleton : pizzas}</div>
+      <Pagination onChange={(number) => setCurrentPage(number)} />
     </div>
   );
 };
