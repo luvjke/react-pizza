0b5e6285653d2cd3e96e0abc627c7c62e292d249
Nikita <nik_antonov_555@mail.ru>
2023-03-17 19:53:12 +0400

    add context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,10 @@ import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import Pagination from '../components/Pagination';
+import { SearchContext } from '../App';
 
-const Home = ({ seacrhValue }) => {
+const Home = () => {
+  const { seacrhValue } = React.useContext(SearchContext);
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategotyId] = React.useState(0);
