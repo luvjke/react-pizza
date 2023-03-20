d2d65159abebcad69df40e3ce3d75021f082cbc1
Nikita <nik_antonov_555@mail.ru>
2023-03-20 10:25:12 +0400

    add slice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { decrement, increment } from './redux/slices/filterSlice';
+
 import './scss/app.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -12,18 +11,9 @@ export const SearchContext = React.createContext();
 
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
 
   return (
     <div className="wrapper">
-      <button aria-label="Increment value" onClick={() => dispatch(increment())}>
-        Increment
-      </button>
-      <span>{count}</span>
-      <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
-        Decrement
-      </button>
       <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
         <div className="content">
